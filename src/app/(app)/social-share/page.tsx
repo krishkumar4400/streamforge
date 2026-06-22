42d073fcb00adbbf9faeca0f3d46871bbb06db0a
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-06-22 10:55:54 +0530

    feat: Add image download page

--- src/app/(app)/social-share/page.tsx
+++ src/app/(app)/social-share/page.tsx
@@ -1,8 +1,82 @@
+"use client"
+
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
+
+const socialFormats = {
+  "Instagram Square (1:1)": { width: 1080, height: 1080, aspectRatio: "1:1" },
+  "Instagram Portrait (4:5)": { width: 1080, height: 1350, aspectRatio: "4:5" },
+  "Twitter Post (16:9)": { width: 1200, height: 675, aspectRatio: "16:9" },
+  "Instagram Square (3:1)": { width: 1500, height: 500, aspectRatio: "3:1" },
+  "Instagram Square (205:78)": { width: 820, height: 312, aspectRatio: "205:78" },
+}
+
+type SocialFormat = keyof typeof socialFormats;
 
 const Page = () => {
+
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [selectedFormat, setSelectedFormat] = useState<SocialFormat>("Instagram Square (1:1)");
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [isTransforming, setIsTransforming] = useState<boolean>(false);
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (!file) return;
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const { data } = await axios.post('/api/image-upload', { formData });
+      if (data.success) {
+        setUploadedImage(data.publicId);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to upload image");
+    } finally {
+      setIsUploading(false);
+    }
+  }
+
+  const handleDownload = () => {
+    if (!imageRef.current) {
+      return;
+    }
+
+    fetch(imageRef.current.src).then((response) => response.blob()).then((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = `${selectedFormat.replace(/\s+/g, "_").toLowerCase()}.png`;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
+    });
+  }
+
+  useEffect(() => {
+    if (uploadedImage) {
+      setIsTransforming(true);
+    }
+  }, [selectedFormat, uploadedImage]);
+
+
+
+
+
+
   return (
     <div>
-      
+
     </div>
   )
 }
